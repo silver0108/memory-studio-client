84584961e8380d369d054a81a6faafa8cc9dd1f7
silver0108 <kk1170@naver.com>
2023-12-27 08:54:09 +0900

    feat: 보관함 구현

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react'
+import Header from '../../components/common/Header'
 import './Gallery.css'
+import axios from 'axios';
 
 export default function Gallery() {
+  const [imgList, setImgList] = useState([]);
+
+  useEffect(()=> {
+    const fetchData = async () => {
+      try{
+        const response = await axios.get(`http://3.36.63.145:8080/api/gallery?userId=${1}`)
+        setImgList(response.data)
+      }catch(error) {
+        console.log(error)
+      }
+    }
+    fetchData();
+  }, [])
+
   return (
-    <div></div>
+    <div className='GalleryWrapper'>
+      <Header/>
+      <div className='GalleryContainer'>
+        <div className='text'>보관함</div>
+          <div className='imgContainer'>
+            {imgList.map((img, index)=>(
+              <img key={index} src={img.url} alt={img.url} 
+                style={{ width: "75vw", height: "auto", margin:"20px"}}/>
+            ))}
+          </div>
+      </div>
+    </div>
   )
 }
